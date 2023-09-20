1df7d342318326506c8bdfac49fe76cea03a5327
AlexisFlc <falconnet.alexis@gmail.com>
2023-09-20 11:19:02 +0200

    question_2_1

--- app_tp1/src/app/page.tsx
+++ app_tp1/src/app/page.tsx
@@ -1,8 +1,40 @@
 import Image from 'next/image'
+import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+import {BreadCrumbs, Button, Heading, SectionContainer} from "tp-kit/components";
+import {ProductCardLayout, ProductGridLayout} from "tp-kit/components/products";
+const categories = PRODUCTS_CATEGORY_DATA;
 
 export default function Home() {
   return (
     <main>
+        <BreadCrumbs
+            items={[
+                {
+                    label: 'Acceuil',
+                    size: 'large',
+
+                    url: '#'
+                }
+            ]}
+        />
+        <SectionContainer>
+            {categories.map(category => (
+                <div key={category.id}>
+                    <BreadCrumbs items={[
+                        {
+                            label: category.name + ' (' + category.products.length + ')',
+                            url: '#'
+                        }
+                    ]}/>
+                    <ProductGridLayout products={category.products}>
+                        {product => (
+                            <ProductCardLayout product={product} button={<Button fullWidth variant="ghost">Ajouter au panier</Button>} />
+                        )}
+                    </ProductGridLayout>
+                </div>
+            ))}
+        </SectionContainer>
+
 
     </main>
   )
