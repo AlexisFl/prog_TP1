b80cd189e10f654b75d57608eca3c313c3198b0e
af303746 <alexis.falconnet@etu.u-bourgogne.fr>
2023-09-26 15:31:47 +0200

    changement_salle

--- app_tp1/src/app/dev-cart/page.tsx
+++ app_tp1/src/app/dev-cart/page.tsx
@@ -2,6 +2,7 @@
 import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { Button, ProductCardLayout, SectionContainer } from "tp-kit/components";
 import {ProductCartLine} from "tp-kit/components/products/product-cart-line";
+import {FormattedPrice} from "tp-kit/components/data-display";
 const products = PRODUCTS_CATEGORY_DATA[0].products.slice(0, 3);
 
 
@@ -12,7 +13,7 @@ export default function DevCartPage() {
             wrapperClassName="flex flex-col lg:flex-row gap-24"
         >
             {/* Produits */}
-            <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 flex-1">
+            {/*<section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 flex-1">
                 {products.map((product) => (
                     <ProductCardLayout
                         key={product.id}
@@ -20,19 +21,35 @@ export default function DevCartPage() {
                         button={<Button variant={"ghost"} fullWidth>Ajouter au panier</Button>}
                     />
                 ))}
-            </section>
+            </section>*/}
             {/* /Produits */}
 
             {/* Panier */}
             <section className="w-full lg:w-1/3 space-y-8">
+                <h2 className="text-lg font-semibold mb-8 tracking-tight">Mon Panier</h2>
                 <div className="flex justify-between items-center">
-                    <span>Produit 1</span>
-                    <span>Quantité : 1</span>
+                    <span className="text-lg font-semibold">
+                        <ProductCartLine
+                            product={products[0]}
+                            qty={1}
+                        />
+                    </span>
                 </div>
                 <div className="flex justify-between items-center">
-                    <span>Produit 2</span>
-                    <span>Quantité : 2</span>
+                    <span className="text-lg font-semibold">
+                        <ProductCartLine
+                            product={products[1]}
+                            qty={2}
+                        />
+                    </span>
                 </div>
+                <div className="flex justify-between items-center">
+                    <span className="text-lg font-semibold">Total</span>
+                    <span className="text-lg font-semibold">
+                        <FormattedPrice price={products[0].price + products[1].price * 2} />
+                    </span>
+                </div>
+
                 <Button variant={"outline"} fullWidth>Vider le panier</Button>
             </section>
             {/* /Panier */}
